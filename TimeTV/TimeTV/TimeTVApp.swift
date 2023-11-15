1afbc2dcc5fa7eec6e86978326d082fb350a2ac8
Anna Cantiello <anna.cant1999@gmail.com>
2023-11-15 10:29:23 +0100

    First Push

---
+++ TimeTV/TimeTV/TimeTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTVApp.swift
+//  TimeTV
+//
+//  Created by Anna Cantiello  on 15/11/23.
+//
+
+import SwiftUI
+
+@main
+struct TimeTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
